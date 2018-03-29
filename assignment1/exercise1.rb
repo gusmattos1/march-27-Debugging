8fecca83a2e531eec6dd640979c02d83a99d6cc9
Gustavo de Mattos <gdm_pg@hotmail.com>
2018-03-28 23:47:57 -0400

    exercise 1 done

--- assignment1/exercise1.rb
+++ assignment1/exercise1.rb
@@ -1,4 +1,4 @@
-emotions = {love: 2, hangry: 1, joy: 3}
+emotions = {love: 1, hangry: 2, joy: 3}
 
 class Person
 
@@ -7,9 +7,22 @@ class Person
     @emotions = emotions
   end
 
+  def meter(emotions)
 
+  emotions.each do |x,y|
+      if y == 1
+        puts "I am feeling a high amount of #{x}"
+      elsif y == 2
+        puts "I am feeling a medium amount of #{x}"
+      elsif y == 3
+        puts "I am feeling a low amount of #{x}"
+      else
+        puts  "Error"
+      end
+    end
+  end
 end
 
 person1 = Person.new("eu",emotions)
 
-puts person1.inspect
+person1.meter(emotions)
